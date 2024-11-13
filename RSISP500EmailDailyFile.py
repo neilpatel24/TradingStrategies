fcf37d702dac4bf60fe53e0935ebd1abae33c327
neilpatel24 <neil_patel24@hotmail.co.uk>
2024-11-13 17:27:04 +0000

    Update RSISP500EmailDailyFile.py

--- RSISP500EmailDailyFile.py
+++ RSISP500EmailDailyFile.py
@@ -8,6 +8,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
+from datetime import datetime
 
 # Function to calculate RSI
 def calculate_rsi(data, period=14):
